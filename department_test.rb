b11cb844fc35c97a8a2436baab7a084d39fb7c74
Benjamin Mangelsen <bmangelsen@lmao.local>
2016-10-20 14:02:12 -0400

    Added test to add employee to department employees array

--- department_test.rb
+++ department_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require_relative './department'
+require_relative './employee'
 
 class DepartmentTest < Minitest::Test
 
@@ -8,13 +9,24 @@ class DepartmentTest < Minitest::Test
       :alpha
   end
 
+  def bob
+    bob = Employee.new("Bob Smith", "bsmith@gmail.com", "864-555-9999", 50000)
+  end
+
+  def finance
+    finance = Department.new("Finance")
+  end
+
   def test_class_exists
     assert Department
   end
 
   def test_class_initialize
-    finance = Department.new("Finance")
     assert_equal "Finance", finance.department_name
+  end
+
+  def test_add_employees
+    assert_equal ["Bob Smith", "bsmith@gmail.com", "864-555-9999", 50000], finance.add_employee_to_department(bob)
   end
 
 end
