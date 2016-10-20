c315f21f14a7821334fefca75d6cc01579b43133
Benjamin Mangelsen <bmangelsen@lmao.local>
2016-10-20 16:48:58 -0400

    Added a test to confirm raise distribution based on performance

--- department_test.rb
+++ department_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'pry'
 require_relative './department'
 require_relative './employee'
 
@@ -10,11 +11,11 @@ class DepartmentTest < Minitest::Test
   end
 
   def bob
-    bob = Employee.new("Bob Smith", "bsmith@gmail.com", "864-555-9999", 50000)
+    @bob = @bob || Employee.new("Bob Smith", "bsmith@gmail.com", "864-555-9999", 50000)
   end
 
   def jill
-    jill = Employee.new("Jill Potts", "jpotts@gmail.com", "864-555-8888", 55000)
+    @jill = @jill || Employee.new("Jill Potts", "jpotts@gmail.com", "864-555-8888", 55000)
   end
 
   def finance
@@ -29,16 +30,22 @@ class DepartmentTest < Minitest::Test
     assert_equal "Finance", finance.department_name
   end
 
-  def test_add_employees
-    assert_equal [bob.employee_info], finance.add_employee(bob.employee_info)
-    assert_equal [bob.employee_info, jill.employee_info], finance.add_employee(jill.employee_info)
-    assert_equal 2, finance.employees.size
+  def test_add_employee_to_department
+    assert_equal [bob], finance.add_employee(bob)
+    assert_equal [bob, jill], finance.add_employee(jill)
+    assert_equal 2, finance.department_employees.size
   end
 
   def test_get_total_salary
-    assert_equal [bob.employee_info], finance.add_employee(bob.employee_info)
-    assert_equal [bob.employee_info, jill.employee_info], finance.add_employee(jill.employee_info)
+    assert_equal [bob], finance.add_employee(bob)
+    assert_equal [bob, jill], finance.add_employee(jill)
     assert_equal 105000, finance.sum_all_salaries
+  end
+
+  def test_distribute_raises
+    assert_equal [bob], finance.add_employee(bob)
+    assert_equal [bob, jill], finance.add_employee(jill)
+    assert_equal [70000, 55000], finance.give_department_raises(20000)
   end
 
 end
