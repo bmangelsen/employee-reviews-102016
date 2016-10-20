aae1c2d7c203712a3fdf8f6195efc210c7dcc6a8
Benjamin Mangelsen <bmangelsen@lmao.local>
2016-10-20 13:31:18 -0400

    Added test to determine initialize successful

--- employee_test.rb
+++ employee_test.rb
@@ -12,4 +12,12 @@ class EmployeeTest < Minitest::Test
     assert Employee
   end
 
+  def test_class_initialize
+    bob = Employee.new("Bob Smith", "bsmith@gmail.com", "864-555-9999", 50000)
+    assert_equal "Bob Smith", bob.name
+    assert_equal "bsmith@gmail.com", bob.email
+    assert_equal "864-555-9999", bob.phone_number
+    assert_equal 50000, bob.salary
+  end
+
 end
