979c19d13effb4374da8a82771ea966bb492d940
Benjamin Mangelsen <bmangelsen@lmao.local>
2016-10-20 14:47:22 -0400

    Added employees variable and add_employee method to add employee to department

--- department.rb
+++ department.rb
@@ -1,9 +1,16 @@
+require_relative './employee'
+
 class Department
 
-  attr_accessor :department_name
+  attr_accessor :department_name, :employees
 
   def initialize(department_name)
     @department_name = department_name
+    @employees = []
+  end
+
+  def add_employee(employee_info)
+    employees << employee_info
   end
 
 end
