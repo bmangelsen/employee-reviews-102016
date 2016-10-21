acbb1fde139989d1257e559d121a4dc9135b7be4
Benjamin Mangelsen <bmangelsen@lmao.local>
2016-10-21 10:25:25 -0400

    Added method give_review to apply a review to employee and store it in review_text

--- employee.rb
+++ employee.rb
@@ -1,22 +1,30 @@
 class Employee
 
-  attr_accessor :name, :email, :phone_number, :salary
+  attr_accessor :name, :email, :phone_number, :salary, :review_text
+  attr_reader :review_response
 
   def initialize(name, email, phone_number, salary)
     @name = name
     @email = email
     @phone_number = phone_number
     @salary = salary
+    @review_text = ""
+    @review_response = ""
   end
 
-  def review_response(review)
+  def give_review(review)
+    review_text = review
+  end
+
+  def determine_review_response(review)
     positive_keywords = ["asset", "pleasure", "happy"]
     negative_keywords = ["confusion", "negative", "inadequate"]
     if review.match(positive_keywords.join("|"))
-      "satisfactory"
+      review_response = "satisfactory"
+    elsif review.match(negative_keywords.join("|"))
+      review_response = "unsatisfactory"
     else
-      review.match(negative_keywords.join("|"))
-      "unsatisfactory"
+      nil
     end
   end
 
